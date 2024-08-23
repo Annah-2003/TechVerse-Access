dc34bdb07c590a25678818b93b3851c5ba86c3e6
Annah-2003 <irenengitau@gmail.com>
2024-08-22 22:18:20 -0700

    created community page

--- tech-event-frontend/pages/communities.js
+++ tech-event-frontend/pages/communities.js
@@ -1,23 +1,42 @@
-import { useEffect, useState} from 'react';
+// pages/communities.js
+
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSession } from 'next-auth/client';
 
-const backendUrl = 'http://localhost:8000/api'
+const backendUrl = 'http://localhost:8000/api';
 
 export default function Communities() {
-    const [communities, setCommunities] = useState([])
-    const [session ] = useSession();
+  const [session] = useSession();
+  const [communities, setCommunities] = useState([]);
 
-    useEffect(() => {
-        if (session) {
-            axios
-            .get(`${backendUrl}/communities`, {  headers: {
-                Authorization: `Token ${session.accessToken}`,
-              },
-            })
-            .then((response) => {
-              setCommunities(response.data);
-            });
-        }
-    }, [session]);
+  useEffect(() => {
+    if (session) {
+      axios
+        .get(`${backendUrl}/communities/`, {
+          headers: {
+            Authorization: `Token ${session.accessToken}`,
+          },
+        })
+        .then((response) => {
+          setCommunities(response.data);
+        });
+    }
+  }, [session]);
+
+  if (!session) {
+    return <div>Please login to view communities.</div>;
+  }
+
+  return (
+    <div>
+      <h1>Recommended Communities</h1>
+      {communities.map((community) => (
+        <div key={community.id}>
+          <h2>{community.name}</h2>
+          <p>{community.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
