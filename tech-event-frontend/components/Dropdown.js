5cf72bafde39e27a87be2d14232da6e4b6c472fa
Annah-2003 <irenengitau@gmail.com>
2024-08-23 22:57:44 -0700

    added stripe token

---
+++ tech-event-frontend/components/Dropdown.js
@@ -0,0 +1,33 @@
+// components/Dropdown.js
+import { useState } from 'react';
+
+const Dropdown = ({ label, options, onSelect }) => {
+  const [selected, setSelected] = useState(options[0]);
+
+  const handleSelect = (option) => {
+    setSelected(option);
+    onSelect(option);
+  };
+
+  return (
+    <div className="relative">
+      <label className="block text-gray-700">{label}</label>
+      <div className="border rounded-lg p-2 cursor-pointer bg-white shadow-lg">
+        <span>{selected}</span>
+      </div>
+      <div className="absolute w-full border rounded-lg bg-white shadow-lg mt-1">
+        {options.map((option, index) => (
+          <div
+            key={index}
+            className="p-2 hover:bg-gray-100 cursor-pointer"
+            onClick={() => handleSelect(option)}
+          >
+            {option}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
